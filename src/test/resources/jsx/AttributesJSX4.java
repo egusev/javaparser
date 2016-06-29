b4caf2da9b098c56d8daf432f555a75c2bdc3079
Eugene Gusev <egusev@gmail.com>
2016-06-29 23:27:01 +0300

    improve test case

--- src/test/resources/jsx/AttributesJSX4.java
+++ src/test/resources/jsx/AttributesJSX4.java
@@ -3,7 +3,7 @@ package jsx;
 public class AttributesJSX4 {
     public void main(String[] arg) {
         <Tag ns:attr1='aaaa' sss:attr2="bbb"
-            aaa:attr3='cccc' attr4={5+4}
+            aaa.bbb.ccc.attr3='cccc' attr4={5+4}
         />
         return;
     }
